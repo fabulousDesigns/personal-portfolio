f5207b4a360a1112f561800ea397a5e606a5509d
Bernard maina <87483225+fabulousDesigns@users.noreply.github.com>
2021-09-20 10:15:40 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('#site-main #testimonials .owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        560: {
+            items: 2
+        }
+    }
+})
