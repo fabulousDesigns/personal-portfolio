5c6e93fba7d2530c001e9c8217f3abac0f2501c7
Bernard maina <87483225+fabulousDesigns@users.noreply.github.com>
2021-09-23 13:53:02 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,4 +10,16 @@ $('#site-main #testimonials .owl-carousel').owlCarousel({
             items: 2
         }
     }
-})
+});
+
+//hire button
+let BTN = document.getElementById("hireBtn");
+
+BTN.addEventListener("click", () => {
+    alert("Get in Touch To hire me 😊✌\n✉ Email :  hazardmburu10@gmail.com\n 📞 Telephone :  +254110026199");
+});
+//footer hire btn
+let btn = document.getElementById("Btn");
+btn.addEventListener("click", () => {
+    alert("Get in Touch To hire me 😊✌\n✉ Email :  hazardmburu10@gmail.com\n 📞 Telephone :  +254110026199");
+});
